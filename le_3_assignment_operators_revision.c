c689d36ff405b97884934a8a3f9c39b40aaa253a
CodeMasterMitesh <prajapati.mitesh461@gmail.com>
2024-12-21 18:31:36 +0530

    assignment and comparison operators

---
+++ le_3_assignment_operators_revision.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main() {
+    // assignment operators
+
+    // =   // assignment
+    // +=  // addition assignment
+    // -=  // subtraction assignment
+    // *=  // multiplication assignment
+    // /=  // division assignment
+    // %=  // modulus assignment
+
+    // assignment
+    int abc = 25;
+    int xyz = 15;
+    float result;
+
+    // addition assignment example
+    //abc += xyz; // abc = 25 + 15;
+    // printf("abc = %d\n", abc);
+
+    // subtraction assignment example
+    //abc -= xyz; // abc = abc - xyz;
+    // printf("abc = %d\n", abc);
+
+    // multiplication assignment example
+    //abc *= xyz; // abc = abc * xyz;
+    // printf("abc = %d\n", abc);
+
+    // division assignment example
+    //abc /= xyz; // abc = abc / xyz;
+
+    // result = (float) abc / xyz;
+    // printf("abc = %.2f\n", result);
+
+    // modulus assignment example
+    //abc %= xyz; // abc = abc % xyz;
+    // printf("abc = %d\n", abc);
+    return 0;
+}
---
+++ le_4_comparison_operators_revision.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+int main() {
+
+    // comparison operators
+    // ans true or false
+    //  ==  // equal to
+    //  !=  // not equal to
+    //  >   // greater than
+    //  <   // less than
+    //  >=  // greater than or equal to
+    //  <=  // less than or equal to
+    
+    //true and false are represented by 1 and 0 respectively and boolean data type is not available in C programming language.
+
+    // <  less than operator
+
+    int a = 46;
+    int b = 45;
+    int c = 48;
+
+    //c = a < b; // true or false // 1 or 0
+    //printf("c = %d", c);
+
+    // <= less than or equal to operator
+    //c = a <= b;
+    //printf("c = %d", c);
+
+    // > greater than operator
+    // c = a > b;
+    // printf("c = %d", c);
+
+    // >= greater than or equal to operator
+    // c = a >= b;
+    // printf("c = %d", c);
+
+    // == equal to operator
+    // c = a == b;
+    // printf("c = %d", c);
+
+    // != not equal to operator
+    // c = a != b;
+    // printf("c = %d", c);
+
+    // int r = a > b < c; 
+    // printf("r = %d", r);
+    return 0;
+}
