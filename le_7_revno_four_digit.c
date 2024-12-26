0ebe4f19a74eb652f5a8927bf49348eca6fdef72
CodeMasterMitesh <prajapati.mitesh461@gmail.com>
2024-12-26 08:54:52 +0530

    update

--- le_7_revno_four_digit.c
+++ le_7_revno_four_digit.c
@@ -2,21 +2,21 @@
 
 int main() {
 
-     int num = 1234,rev;
+     int num,rev;
      printf("Enter a four digit number: ");
      scanf("%d",&num);
 
-     rev = num % 10; // 1234 % 10 = 4
-     printf("%d",rev);
-     
-     num = num / 10; // 1234 / 10 = 123
-     rev = num % 10; // 123 % 10 = 3
-     printf("%d",rev);
+     rev = num % 10;  //1234 % 10 = 4
+     printf("%d",rev); //print 4 
+     num = num / 10; //1234 / 10 = 123
 
-     num = num / 10; // 123 / 10 = 12
+     rev = num % 10;  // 123 % 10 = 3
+     printf("%d",rev); //print 3
+     num = num / 10;// 123 / 10 = 12
+
      rev = num % 10; // 12 % 10 = 2
-     printf("%d",rev);
-     rev = num / 10; // ans = 1
-     printf("%d",rev);
+     printf("%d",rev); // print 2
+     rev = num / 10; // 12 / 10 = 1
+     printf("%d",rev); //print 1
     return 0;
 }
