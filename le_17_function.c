3b8da3bd257ba1c1681a494b41cad45e9ad265ee
CodeMasterMitesh <prajapati.mitesh461@gmail.com>
2025-01-16 09:10:17 +0530

    update

---
+++ le_17_function.c
@@ -0,0 +1,24 @@
+// what is function?
+// block of code using function we create reuseable code.
+// function 
+
+#include <stdio.h>
+
+// prototype 
+//datatype functioName parantheses semicolun
+void msg();
+
+// function define
+void msg()
+{
+  printf("Welcome Patel Web Solution\n");     
+}
+
+int main() {
+
+    // function calling
+    msg(); 
+    msg();
+    msg();
+    return 0;
+}
