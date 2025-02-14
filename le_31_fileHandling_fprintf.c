d7a8467df874bdf1fd6b3a29e0902347f41d005b
CodeMasterMitesh <prajapati.mitesh461@gmail.com>
2025-02-14 08:57:07 +0530

    update

---
+++ le_31_fileHandling_fprintf.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+// fgetc,fgets
+int main() {
+    system("cls");
+    FILE *fp;
+    
+    char name[100];
+
+    fp = fopen("students.txt","r");
+
+    // fputs(name,fp);
+    while (fgets(name,99,fp) != NULL)
+    {
+        printf("%s",name);
+    }
+
+
+    fclose(fp);
+   
+    return 0;
+}
