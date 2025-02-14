6d3cf3c002c253f665d592d3ccaaa7097701bcd4
CodeMasterMitesh <prajapati.mitesh461@gmail.com>
2025-02-14 09:08:57 +0530

    update

--- le_31_fileHandling_fprintf.c
+++ le_31_fileHandling_fprintf.c
@@ -4,19 +4,28 @@
 int main() {
     system("cls");
     FILE *fp;
-    
-    char name[100];
 
-    fp = fopen("students.txt","r");
+    int x,y,z;
 
-    // fputs(name,fp);
-    while (fgets(name,99,fp) != NULL)
-    {
-        printf("%s",name);
+    printf("Enter Value for x and y :");
+    scanf("%d",&x);
+    scanf("%d",&y);
+
+    z = x + y;
+
+    fp = fopen("sum.txt","w+");
+
+    if(fp == NULL){
+        printf("File Not Exist");
+        exit(1);
     }
 
+    fprintf(fp,"Sum of x = %d and y = %d is %d",x,y,z);
+    // fprintf(fp,"My Name Is Mitesh.");
+    
 
     fclose(fp);
    
     return 0;
 }
+
